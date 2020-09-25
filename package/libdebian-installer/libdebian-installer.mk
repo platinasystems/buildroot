71ea52ac082ce5d13f711d47ca1ff013e8b98f47
Kevin Paul Herbert <kph@platinasystems.com>
2020-09-24 17:00:25 -0700

    libdebian-installer: Make it work
    Fix bugs and update version from patches found in Buildroot mailing list.
    
    Signed-off-by: Kevin Paul Herbert <kph@platinasystems.com>

--- package/libdebian-installer/libdebian-installer.mk
+++ package/libdebian-installer/libdebian-installer.mk
@@ -5,9 +5,9 @@
 ################################################################################
 
 
-LIBDEBIAN_INSTALLER_VERSION = 0.104
+LIBDEBIAN_INSTALLER_VERSION = 0.120
 LIBDEBIAN_INSTALLER_SOURCE = libdebian-installer_$(LIBDEBIAN_INSTALLER_VERSION).tar.xz
-LIBDEBIAN_INSTALLER_SITE = http://snapshot.debian.org/archive/debian-debug/20160218T034808Z/pool/main/libd/libdebian-installer/
+LIBDEBIAN_INSTALLER_SITE = http://deb.debian.org/debian/pool/main/libd/libdebian-installer
 LIBDEBIAN_INSTALLER_LICENSE = GPLv2
 LIBDEBIAN_INSTALLER_LICENSE_FILES = debian/copyright
 LIBDEBIAN_INSTALLER_INSTALL_STAGING = YES
